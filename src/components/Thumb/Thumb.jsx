df5bcb35d30007822571e3663d5779ef83249a6d
Emmeline DOHY <emmeline.dohypgmail.com>
2023-08-17 18:19:41 +0200

    update FicheLogement + Thumb

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Thumb({id,cover,title}) {
     return (
         <div className="ThumbContainer">
-            <Link  to={`/fiche-logement/${id}`}>
+            <Link  to={`/FicheLogment/${id}`}>
                 <img className="ThumbContainerImg" src={cover} alt={title} />
                 <h2 className="ThumbContainerTitle"> {title} </h2>
             </Link>
--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -8,20 +8,75 @@ import Rating from '../../components/Rating/Rating.jsx';
 
 
 
-export default function Fiche_logement() {
+export default function FicheLogement() {
+    // identifying the accomodation ID from the Url settings
+    const { id } = useParams()
+
+    // Find the accomodation corresponding in the list
+    const accomodation = list.find(accomodation => accomodation.id === id)
+
+    // If the accomodation doesn't exist, redirect to error page
+    if (!accomodation) {
+    return <Navigate to="/404" />
+    }
+
+    //Recovery of accomodation values to store them in variables
+    const tags = accomodation.tags
+    const equipements = accomodation.equipments
+    const hostPicture = accomodation.host.picture
+    const picturesList = accomodation.pictures
+    const picturesLength = accomodation.pictures.length
+    const ratingNumber = accomodation.rating
+
     return (
-        <>
-            <div className="ficheLogementContainer">
-                <Carrousel/>
-                <h1>Title{/*{Logements.filter(l=>l.id === post.logementId)[0].title}*/}</h1>
-                <span>Location</span>
-                <span>host</span>
-                <image>hostPicture</image>
-                <span>rating</span>
-                <div className="fiche_LogementDescription"></div>
-                <div className="fiche_LogementEquipements"></div>
+    <main className="accomodationContainer}">
+    
+        <Carrousel pictures={picturesList} picturesLength={picturesLength}/>
+        
+        <article className="accomodationDetails">
+            <div>
+                <h1 className="accomodationTitle">{accomodation.title}</h1>
+                <p className="accomodationLocation">{accomodation.location}</p>
+
+                <div className="tagsContainer">
+                    {tags.map((tags, index) => (
+                    <Tag key={`${index}-${tags}`} tagName={tags} />
+                    ))}
+                </div>
             </div>
-            
-        </>
+
+            <div className="hostRatingContainer">
+                <div className="host">
+                    <p className="hostName">{accomodation.host.name}</p>
+                    <img className="hostPicture" src={hostPicture} alt={accomodation.host.name} />
+                </div>
+                <div>
+                    <Rating ratingNumber={ratingNumber}/>
+                </div>
+            </div>
+        </article>
+
+        <article className="dropdownContainer">
+            <div className="dropdownDescription">
+                <Dropdown 
+                    DropdownTitle={<div className="accommodationDropdownTitle">Description</div>}
+                    DropdownDescription={<div className= "accommodationDropdownDescription">{accomodation.description}</div>} 
+                />
+            </div>
+
+            <div className="dropdownEquipment">
+                <Dropdown 
+                    collapseTitle={<div className="accommodationDropdownTitle">Equipements</div>}
+                    collapseDescription={
+                    <ul className="equipmentContainer">
+                        {equipements.map((equipment) => (
+                        <li key={`${accomodation.id}-${equipment}`} className="accomodationEquipmentsList">{equipment}</li>
+                        ))}
+                    </ul>
+                    }
+                />
+            </div>
+        </article>
+    </main>
     )
 }
